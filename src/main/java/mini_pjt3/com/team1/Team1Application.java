a5ef6237d6c73c7e4e2984ce257298ee20225b98
hongjiho5148 <wlgh51485148@gmail,com>
2026-05-07 15:18:44 +0900

    'set'

--- src/main/java/mini_pjt3/com/team1/Team1Application.java
+++ src/main/java/mini_pjt3/com/team1/Team1Application.java
@@ -2,6 +2,7 @@ package mini_pjt3.com.team1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Team1Application {
