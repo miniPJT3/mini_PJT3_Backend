7038881550246c0c9c515641a423a8d02f26b1e3
hongjiho5148 <wlgh51485148@gmail,com>
2026-05-07 17:14:38 +0900

    entity&enum add

--- src/main/java/mini_pjt3/com/team1/entity/Member.java
+++ src/main/java/mini_pjt3/com/team1/entity/Member.java
@@ -1,4 +1,34 @@
 package mini_pjt3.com.team1.entity;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.*;
+import mini_pjt3.com.team1.enums.Role;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String username; // 일반 로그인 아이디
+    private String password; // 암호화된 비밀번호
+    private String name;     // 이름 또는 닉네임
+    private String provider; // google, kakao 등
+
+    @Enumerated(EnumType.STRING)
+    private Role role;       // USER, SELLER, ADMIN
+
+    @Builder
+    public Member(String email, String username, String password, String name, String provider, Role role) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.provider = provider;
+        this.role = role;
+    }
 }
--- src/main/java/mini_pjt3/com/team1/enums/AccountStatus.java
+++ src/main/java/mini_pjt3/com/team1/enums/AccountStatus.java
@@ -1,4 +1,7 @@
 package mini_pjt3.com.team1.enums;
 
 public enum AccountStatus {
+    ACTIVE,    // 사용 가능 (입금 대기)
+    USED,      // 입금 완료되어 사용 종료
+    EXPIRED    // 3시간 경과로 인한 무효화
 }
